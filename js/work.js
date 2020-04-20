98a1f776e72756e2af38fc1e4bda0afe491b992b
unknown <mriganka.office55@gmail.com>
2020-04-20 23:06:47 +0530

    some hover effect to the grid items

--- js/work.js
+++ js/work.js
@@ -1,108 +1,134 @@
+// const fltrBtn = {
+//   all: document.querySelector(".all"),
+//   dzukou: document.querySelector(".dzukou"),
+//   sunset: document.querySelector(".sunset"),
+// };
 
-const fltrBtn={
-    all: document.querySelector(".all"),
-    dzukou: document.querySelector(".dzukou"),
-    sunset: document.querySelector(".sunset"),
-}
+//my workaround
+// iso.arrange({ filter: '.sunset' });
 
-var elem = document.querySelector('.grid');
+//my initial code to handle button click
+
+// const fltrBtnHandler= (param)=>{
+//     const fltr = `.${param}`
+//     iso.arrange({
+//     filter: fltr,
+//     sortBy: 'number'
+//   })
+//   // re-apply filtering, sorting, and layout
+//   iso.arrange();
+// }
+
+// fltrBtn.all.addEventListener("click", ()=>{
+//     fltrBtnHandler("all");
+// })
+
+// fltrBtn.dzukou.addEventListener("click", ()=>{
+//     fltrBtnHandler("dzukou");
+// })
+// fltrBtn.sunset.addEventListener("click", ()=>{
+//     fltrBtnHandler("sunset");
+// })
 
 // in case layout to be refreshed after all the images are loaded
-        // imagesLoaded( elem, function() {
-        //     // init Isotope after all images have loaded
-        //     iso = new Isotope( elem, {
-        //       itemSelector: '.grid-item',
-        //       percentPosition: true,
-        //       masonry: {
-        //         columnWidth: 100
-        //       }
-        //     });
-        //   });
+// imagesLoaded( elem, function() {
+//     // init Isotope after all images have loaded
+//     iso = new Isotope( elem, {
+//       itemSelector: '.grid-item',
+//       percentPosition: true,
+//       masonry: {
+//         columnWidth: 100
+//       }
+//     });
+//   });
 
-
-var iso = new Isotope( elem, {
-  itemSelector: '.grid-item',
-  stagger: 50,
-  transitionDuration: 300,
+var elem = document.querySelector(".grid");
+var iso = new Isotope(elem, {
+  itemSelector: ".grid-item",
+  stagger: 60,
+  transitionDuration: 400,
   percentPosition: true,
   hiddenStyle: {
     opacity: 0,
-    transform: 'scale(0.001)'
+    transform: "scale(0.001)",
   },
   visibleStyle: {
     opacity: 1,
-    transform: 'scale(1)'
+    transform: "scale(1)",
   },
-//   masonry: {
-//     columnWidth: '.grid-sizer'}
-//   layoutMode: 'fitRows',
-// layoutMode: 'packery',
+  //   masonry: {
+  //   columnWidth: '.grid-sizer'}
+  //   layoutMode: 'fitRows',
+  //   layoutMode: 'packery',
   masonry: {
-      columnWidth: 100,
-      gutter: 0,
-    
-    }
+    columnWidth: 100,
+    gutter: 0,
+  },
+
+  
 });
 
+imagesLoaded(elem).on("progress", function () {
+  // layout Isotope after each image loads
+  iso.layout();
+});
 
-imagesLoaded( elem ).on( 'progress', function() {
-    // layout Isotope after each image loads
-    iso.layout();
+console.log(1);
+
+iso.on("arrangeComplete", function (filteredItems) {
+  console.log('initial layout done...');
+});
+
+const addTransitionToGridItems = () => {
+  iso.on("arrangeComplete", function (filteredItems) {
+    filteredItems.forEach((filteredItem) => {
+      filteredItem.element.classList.add("transition-griditem");
+    });
   });
+};
 
-  //my workaround
-        // iso.arrange({ filter: '.sunset' });
-
-//my initial code to handle button click
-  
-        // const fltrBtnHandler= (param)=>{
-        //     const fltr = `.${param}`
-        //     iso.arrange({
-        //     filter: fltr, 
-        //     sortBy: 'number'
-        //   })
-        //   // re-apply filtering, sorting, and layout
-        //   iso.arrange();
-        // }
-
-        // fltrBtn.all.addEventListener("click", ()=>{
-        //     fltrBtnHandler("all");
-        // })
-
-        // fltrBtn.dzukou.addEventListener("click", ()=>{
-        //     fltrBtnHandler("dzukou");
-        // })
-        // fltrBtn.sunset.addEventListener("click", ()=>{
-        //     fltrBtnHandler("sunset");
-        // })
-
-var filtersElem = document.querySelector('.filters-button-group');
-filtersElem.addEventListener( 'click', function( event ) {
+var filtersElem = document.querySelector(".filters-button-group");
+filtersElem.addEventListener("click", function (event) {
   // only work with buttons
-  if ( !matchesSelector( event.target, 'button' ) ) {
+  if (!matchesSelector(event.target, "button")) {
     return;
   }
-  var filterValue = event.target.getAttribute('data-filter');
+
+  var tranGridItems = document.querySelectorAll(".transition-griditem");
+  tranGridItems.forEach((element) => {
+    element.classList.remove("transition-griditem");
+  });
+
+  var filterValue = event.target.getAttribute("data-filter");
   iso.arrange({ filter: filterValue });
-  var allItems = iso.getItemElements();
-  var displayedItems = iso.getFilteredItemElements();
-  console.log(displayedItems, allItems);
+
+  addTransitionToGridItems();
+
+  // var allItems = iso.getItemElements();
+  // var displayedItems = iso.getFilteredItemElements();
+  // // console.log(displayedItems, allItems);
 });
 
-var buttonGroups = document.querySelectorAll('.button-group');
-for ( var i=0, len = buttonGroups.length; i < len; i++ ) {
+var buttonGroups = document.querySelectorAll(".button-group");
+for (var i = 0, len = buttonGroups.length; i < len; i++) {
   var buttonGroup = buttonGroups[i];
-  radioButtonGroup( buttonGroup );
+  radioButtonGroup(buttonGroup);
 }
 
 // to assign checked class to selected filter button
-function radioButtonGroup( buttonGroup ) {
-  buttonGroup.addEventListener( 'click', function( event ) {
+function radioButtonGroup(buttonGroup) {
+  buttonGroup.addEventListener("click", function (event) {
     // only work with buttons
-    if ( !matchesSelector( event.target, 'button' ) ) {
+    if (!matchesSelector(event.target, "button")) {
       return;
     }
-    buttonGroup.querySelector('.is-checked').classList.remove('is-checked');
-    event.target.classList.add('is-checked');
+    buttonGroup.querySelector(".is-checked").classList.remove("is-checked");
+    event.target.classList.add("is-checked");
   });
 }
+
+
+iso.arrange({ filter: '.dzukou' });
+console.log(2);
+addTransitionToGridItems();
+console.log(3);
