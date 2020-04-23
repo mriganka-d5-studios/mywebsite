5264dcdee3ebdd9b6359de69de483b6b9c3131f2
mriganka-d5-studios <mriganka.office55@gmail.com>
2020-04-23 15:25:21 +0530

    dynamically generated lightgallery added

--- js/work.js
+++ js/work.js
@@ -48,205 +48,110 @@
 // var displayedItems = iso.getFilteredItemElements();
 // // console.log(displayedItems, allItems);
 
-
 //Set State variable===========================================
 const state = {
   layoutCompleted: false,
   defaultFilter: ".dzukou",
   images: [
-    {
-      imgId: "001",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/1thumb.jpg",
-      src: "/img/1.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "002",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/2thumb.jpg",
-      src: "/img/2.jpg",
-      filterTags: "grid-item sunset",
-    },
-    {
-      imgId: "003",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/3thumb.jpg",
-      src: "/img/3.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "004",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/4thumb.jpg",
-      src: "/img/4.jpg",
-      filterTags: "grid-item dzukou",
-    },
-    {
-      imgId: "005",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/5thumb.jpg",
-      src: "/img/5.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "006",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/6thumb.jpg",
-      src: "/img/6.jpg",
-      filterTags: "grid-item sunset",
-    },
-    {
-      imgId: "007",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/7thumb.jpg",
-      src: "/img/7.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "008",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/8thumb.jpg",
-      src: "/img/8.jpg",
-      filterTags: "grid-item dzukou",
-    },
-    {
-      imgId: "009",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/9thumb.jpg",
-      src: "/img/9.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "010",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/10thumb.jpg",
-      src: "/img/10.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "011",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/11thumb.jpg",
-      src: "/img/11.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "012",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/12thumb.jpg",
-      src: "/img/12.jpg",
-      filterTags: "grid-item dzukou",
-    },
-    {
-      imgId: "013",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/13thumb.jpg",
-      src: "/img/13.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "014",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/14thumb.jpg",
-      src: "/img/14.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-    {
-      imgId: "015",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/15thumb.jpg",
-      src: "/img/15.jpg",
-      filterTags: "grid-item sunset",
-    },
-    {
-      imgId: "016",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/16thumb.jpg",
-      src: "/img/16.jpg",
-      filterTags: "grid-item dzukou",
-    },
-    {
-      imgId: "017",
-      isInFiltered: 0,
-      alt: "a moment captured!",
-      thumbSrc: "/img/17thumb.jpg",
-      src: "/img/17.jpg",
-      filterTags: "grid-item dzukou sunset",
-    },
-  ],
+    // {imgId: "001", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/1thumb.jpg", src: "/img/1.jpg", filterTags: "grid-item dzukou", desc:" White Flowers"},
+    // {imgId: "002", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/2thumb.jpg", src: "/img/2.jpg", filterTags: "grid-item sunset", desc:"Brahmaputra Bank"}, 
+    {imgId: "001", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/1thumb.jpg", src: "/img/1.jpg", filterTags: "grid-item dzukou", desc:" White Flowers"},
+    {imgId: "002", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/2thumb.jpg", src: "/img/2.jpg", filterTags: "grid-item sunset", desc:"Brahmaputra Bank"}, 
+    {imgId: "003", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/3thumb.jpg", src: "/img/3.jpg", filterTags: "grid-item sunset", desc:"Phea Phea"}, 
+    {imgId: "004", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/4thumb.jpg", src: "/img/4.jpg", filterTags: "grid-item dzukou", desc:"River side"}, 
+    {imgId: "005", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/5thumb.jpg", src: "/img/5.jpg", filterTags: "grid-item sunset", desc:"Umiam"}, 
+    {imgId: "006", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/6thumb.jpg", src: "/img/6.jpg", filterTags: "grid-item dzukou", desc:"Chandubi"}, 
+    {imgId: "007", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/7thumb.jpg", src: "/img/7.jpg", filterTags: "grid-item dzukou", desc:"Chandubi"}, 
+    {imgId: "008", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/8thumb.jpg", src: "/img/8.jpg", filterTags: "grid-item dzukou", desc:"Menchuka"}, 
+    {imgId: "009", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/9thumb.jpg", src: "/img/9.jpg", filterTags: "grid-item dzukou", desc:"Dainthelan"}, 
+    {imgId: "010", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/10thumb.jpg", src: "/img/10.jpg", filterTags: "grid-item sunset", desc:"Ghost Ship"}, 
+    {imgId: "011", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/11thumb.jpg", src: "/img/11.jpg", filterTags: "grid-item dzukou", desc:"Baby"}, 
+    {imgId: "012", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/12thumb.jpg", src: "/img/12.jpg", filterTags: "grid-item dzukou", desc:"Home Minister"}, 
+    {imgId: "013", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/13thumb.jpg", src: "/img/13.jpg", filterTags: "grid-item dzukou", desc:"Bihu"}, 
+    {imgId: "014", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/14thumb.jpg", src: "/img/14.jpg", filterTags: "grid-item sunset", desc:"Dhuakhua"}, 
+    {imgId: "015", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/15thumb.jpg", src: "/img/15.jpg", filterTags: "grid-item dzukou", desc:"Work"}, 
+    {imgId: "016", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/16thumb.jpg", src: "/img/16.jpg", filterTags: "grid-item dzukou", desc:"Panikhaiti"}, 
+    {imgId: "017", isInFiltered: 0, alt: "a moment captured!", thumbSrc: "/img/17thumb.jpg", src: "/img/17.jpg", filterTags: "grid-item sunset", desc:"Umiam"}, 
+    
+  ],  
   domElements: {
     gridContainer: document.querySelector(".grid"),
-    lightBoxContainer: document.querySelector(".lightboxContainer"),
-    lightBox:document.querySelector(".lightbox"),
+    lg: document.getElementById('dynamic'),
+    btnGrp: document.querySelector(".button-group"),
   },
 };
 
 //add items to DOM============================================
 const addImagesToDOM = () => {
   state.images.forEach((image) => {
-    const markup = `<div id="${image.imgId}" class="${image.filterTags}"><img src="${image.src}" alt="${image.alt}"></div>`;
+    const markup = `<div id="${image.imgId}" class="${image.filterTags}"><img src="${image.src}" 
+                    alt="${image.alt}"></div>`;
     state.domElements.gridContainer.insertAdjacentHTML("beforeend", markup);
   });
   console.log("dom added");
 };
 addImagesToDOM();
 
-
-
 //click event in photos to initialise the lightbox=========================
 
-state.domElements.gridContainer.addEventListener('click', (event)=>{
+state.domElements.gridContainer.addEventListener("click", (event) => {
+  const initImageId = event.path[1].getAttribute("id");
+  let initImageIndex = 0;
+  let dynmcMarkup = []  ;
+  let ind = -1;
 
-  initImage=state.images[parseInt((event.path[1].getAttribute("id")))-1];
+  state.images.forEach(image => {
+      if(image.isInFiltered===1){
+        const mkup={"src":image.src, 'thumb': image.src, 'subHtml': `<h4>${image.alt}</h4><p>${image.desc}</p>`};
+        dynmcMarkup.push(mkup);
+        ind= ind+1 ;
+        if (image.imgId===initImageId) {initImageIndex = ind} ;            
+      }     
+
+  });
   
-  state.domElements.lightBoxContainer.classList.toggle("invisible");
-  state.domElements.lightBox.classList.toggle("invisible");
-  state.domElements.gridContainer.classList.toggle("invisible");
-  const markup=`<img src="${initImage.src}" alt="${initImage.alt}">`
-  // console.log(markup);
-  console.log(event.path[0].naturalHeight);
-  console.log(event.path[0].naturalWidth);
-  state.domElements.lightBox.insertAdjacentHTML('beforeend', markup);
-  let wH=window.innerHeight;
-  let wW=window.innerWidth;
-  console.log(wH,wW);
-})
-
-// click to load next image==============================================
-
-
-
-// click to load next image==============================================
-
-
-
-
-// click to exit lightbox=====================================================
-state.domElements.lightBox.addEventListener('click', (event)=>{
-
+  state.domElements.lg.removeAttribute("lg-uid");
   
-  state.domElements.lightBoxContainer.classList.toggle("invisible");
-  state.domElements.lightBox.classList.toggle("invisible");
-  state.domElements.gridContainer.classList.toggle("invisible");
-  state.domElements.lightBox.innerHTML="";
-})
+  lightGallery(state.domElements.lg, {
+      dynamic: true,
+      dynamicEl: dynmcMarkup,
+      index: initImageIndex,
+
+      mode:'lg-zoom-out-in',
+      download: false,
+      hideBarsDelay:1000,
+  
+      thumbnail:true,
+      thumbMargin:10,
+      fullScreen:true,
+      autoplay:false,
+      zoom:true,
+      share:true,
+      googlePlus:false,
+  });
+
+});
+
+//hide the thumbnails after 3 sec, also hide the grid to avoid scrollbar on right==============
+state.domElements.lg.addEventListener('onBeforeOpen', function(e){
+ 
+  state.domElements.gridContainer.style.display="none";
+  state.domElements.btnGrp.style.display="none";
+  setTimeout(function(){ 
+      const lgOuter =document.querySelector(".lg-outer");
+      if(lgOuter){
+          console.log(lgOuter);
+          lgOuter.classList.remove("lg-thumb-open");
+      }
+  }, 3000);
+}, false);
+
+//when lightbox closes, making grid visible================================
+state.domElements.lg.addEventListener('onBeforeClose', function(e){
+           state.domElements.gridContainer.style.removeProperty("display");
+           state.domElements.btnGrp.style.removeProperty("display");
+ }, false);
+
 //initiate Isotope ===========================================
 var iso = new Isotope(state.domElements.gridContainer, {
   itemSelector: ".grid-item",
@@ -261,10 +166,6 @@ var iso = new Isotope(state.domElements.gridContainer, {
     opacity: 1,
     transform: "scale(1)",
   },
-  //   masonry: {
-  //   columnWidth: '.grid-sizer'}
-  //   layoutMode: 'fitRows',
-  //   layoutMode: 'packery',
   masonry: {
     columnWidth: 100,
     gutter: 0,
@@ -278,33 +179,33 @@ imagesLoaded(state.domElements.gridContainer).on("progress", function () {
   console.log("imageloaded.on progress fired!");
 });
 
-
 //add transition class when the arrngement is completed====================
 //also update images obj in state for inFiltered
+
 iso.on("arrangeComplete", function (filteredItems) {
-  const filteredIndexArray = [];
+  
   console.log("4 on arrangecomplete fired and adding class");
-  filteredItems.forEach((filteredItem) => {
-    filteredItem.element.classList.add("transition-griditem");
 
-    const filteredItemID = filteredItem.element.getAttribute("id");
-
-    const matchIndex = state.images.findIndex((image) => {
-      return image.imgId === filteredItemID;
-    });
-    // console.log(matchIndex);
-    filteredIndexArray.push(matchIndex);
-  });
   state.images.forEach((image) => {
     image.isInFiltered = 0;
-  });
-  filteredIndexArray.forEach((element) => {
-    state.images[element].isInFiltered = 1;
-  });
+  })
 
-  // console.log(state.images);
-  // // console.log(filteredIndexArray);
-  console.log("5 classes added");
+    filteredItems.forEach((filteredItem) => {
+      filteredItem.element.classList.add("transition-griditem");
+
+      const filteredItemID = filteredItem.element.getAttribute("id");
+
+      const indItem = state.images.findIndex((image) => {
+        return image.imgId === filteredItemID;
+      });
+
+      state.images[indItem].isInFiltered=1;
+      // console.log(indItem);
+    })
+
+  console.log(state.images);
+  console.log(filteredItems);
+  console.log("5 classes added and state.images updated");
 
   state.layoutCompleted = true;
   console.log("6 on arrangecomplete completed and state set to true");
@@ -336,8 +237,6 @@ filtersElem.addEventListener("click", function (event) {
   }
 });
 
-
-
 //add is-checked class to present filter tag button=========================
 var buttonGroups = document.querySelectorAll(".button-group");
 for (var i = 0, len = buttonGroups.length; i < len; i++) {
@@ -356,11 +255,8 @@ function radioButtonGroup(buttonGroup) {
   });
 }
 
-
-
 //initialieing....============================================
 window.addEventListener("load", (event) => {
   iso.arrange({ filter: `${state.defaultFilter}` });
   console.log("iso.arrange dzukou");
 });
-
